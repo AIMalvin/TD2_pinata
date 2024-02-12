cc3dd691dfe4ebd6bf48b1663fc5e70efabc5041
Malvin <69598452+AIMalvin@users.noreply.github.com>
2024-02-12 15:50:55 +0100

    Create deploy-to-pinata.js

---
+++ deploy-to-pinata.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+
+const pinFileToIPFS = async () => {
+    const formData = new FormData();
+    const src = "chaton.png"; // This should be dynamically set based on your build output
+    
+    const file = fs.createReadStream(src);
+    formData.append('file', file);
+    
+    const pinataMetadata = JSON.stringify({
+        name: 'File name', // This should be the name of your pin, you can make this dynamic too
+    });
+    formData.append('pinataMetadata', pinataMetadata);
+    
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+    });
+    formData.append('pinataOptions', pinataOptions);
+
+    try {
+        const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+            maxBodyLength: "Infinity",
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+                'Authorization': `Bearer ${process.env.JWT}` // Securely load the JWT from environment variables
+            }
+        });
+        console.log(res.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+pinFileToIPFS();
